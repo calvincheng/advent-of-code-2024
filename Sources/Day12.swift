7dbc73bedcddedf4f9ef94b6f84e124e1c547cb9
Calvin Cheng <calvin.cc.cheng@gmail.com>
2024-12-17 10:23:41 +0800

    feat: day 12 and 13

---
+++ Sources/Day12.swift
@@ -0,0 +1,159 @@
+import Foundation
+
+struct Day12: AdventDay {
+  var data: String
+
+  private let example: String = """
+    OOOOO
+    OXOXO
+    OOOOO
+    OXOXO
+    OOOOO
+    """
+
+  private let example2: String = """
+    AAAA
+    BBCD
+    BBCC
+    EEEC
+    """
+
+  init(data: String) {
+    self.data = example2 // data
+  }
+
+  private func parse(_ data: String) -> [[Character]] {
+      data
+        .split(separator: "\n")
+        .map { row in Array(row) }
+  }
+
+  private let directions: [(dx: Int, dy: Int)] = [
+    (0, -1),
+    (1, 0),
+    (0, 1),
+    (-1, 0),
+  ]
+
+  private func dfs(grid: [[Character]], i: Int, j: Int) -> (area: Int, fences: Set<[Int]>, visited: Set<[Int]>) {
+      let W = grid[0].count
+      let H = grid.count
+      let type: Character = grid[j][i]
+
+      var stack: [[Int]] = [] // [i, j]
+      var visited: Set<[Int]> = []
+      var area: Int = 0
+      var fences: Set<[Int]> = [] // [i, j, direction]
+
+      stack.append([i, j])
+
+      while !stack.isEmpty {
+          let curr = stack.popLast()!
+
+          if visited.contains(curr) { continue }
+          visited.insert(curr)
+
+          let (i, j) = (curr[0], curr[1])
+
+          if grid[j][i] == type {
+              area += 1
+          }
+
+          // Get neighbors
+          let nbrs = directions.enumerated().compactMap { di, d in 
+            // too lazy to turn direction into an enum, just use its index
+            return (i + d.dx, j + d.dy, di)
+          }
+          // Count fences
+          for nbr in nbrs {
+              let (x, y, di) = nbr
+              let inBounds = 0 <= x && x < W && 0 <= y && y < H
+              if !inBounds || grid[y][x] != type {
+                  fences.insert([x, y, di])
+              }
+          }
+          // Add neighbours
+          for nbr in nbrs {
+              let (x, y, _) = nbr
+              let inBounds = 0 <= x && x < W && 0 <= y && y < H
+              if inBounds, grid[y][x] == type {
+                  stack.append([x, y])
+              }
+          }
+      }
+
+      return (area, fences, visited)
+
+  }
+
+  func part1() -> Int {
+    let grid = parse(self.data)
+    let W = grid[0].count
+    let H = grid.count
+    var price: Int = 0
+    var visited: Set<[Int]> = []
+    for j in 0..<H {
+        for i in 0..<W {
+            if visited.contains([i, j]) { continue }
+            let (a, fs, v) = self.dfs(grid: grid, i: i, j: j)
+            price += (a * fs.count)
+            visited = visited.union(v)
+        }
+    }
+    return price
+  }
+
+  func part2() -> Int {
+    let grid = parse(self.data)
+    let W = grid[0].count
+    let H = grid.count
+    var price: Int = 0
+    var visited: Set<[Int]> = []
+    for j in 0..<H {
+        for i in 0..<W {
+            if visited.contains([i, j]) { continue }
+
+            let (a, fs, v) = self.dfs(grid: grid, i: i, j: j)
+            visited = visited.union(v)
+
+            // scan rows and cols and count sides -- can probably use a smart
+            // sorting algo but i'm lazy
+            var numSides: Int = 0
+
+            // check for horizontal fences along each row
+            for dir in [0, 2] {
+                for j in -1..<H+1 { 
+                    var last: [Int]? = nil
+                    for i in -1..<W+1 {
+                        let fence = fs.first { $0 == [i, j, dir] }
+                        if last == nil, fence != nil {
+                            numSides += 1
+                        }
+                        last = fence
+                    }
+                    last = nil
+                }
+            }
+            // check for vertical fences along each column
+            for dir in [1, 3] {
+                for i in -1..<W+1 { 
+                    var last: [Int]? = nil
+                    for j in -1..<H+1 {
+                        let fence = fs.first { $0 == [i, j, dir] }
+                        if last == nil, fence != nil {
+                            numSides += 1
+                        }
+                        last = fence
+                    }
+                    last = nil
+                }
+            }
+
+            // add em up
+            price += (a * numSides)
+        }
+    }
+    // bob's ur uncle
+    return price
+  } 
+}
---
+++ Sources/Day13.swift
@@ -0,0 +1,145 @@
+import Foundation
+
+struct Day13: AdventDay {
+  var data: String
+
+  private let example: String = """
+    Button A: X+94, Y+34
+    Button B: X+22, Y+67
+    Prize: X=8400, Y=5400
+
+    Button A: X+26, Y+66
+    Button B: X+67, Y+21
+    Prize: X=12748, Y=12176
+
+    Button A: X+17, Y+86
+    Button B: X+84, Y+37
+    Prize: X=7870, Y=6450
+
+    Button A: X+69, Y+23
+    Button B: X+27, Y+71
+    Prize: X=18641, Y=10279
+    """
+
+  private let epsilon: Double = 1e-3
+
+  init(data: String) {
+    self.data = data
+  }
+
+  private func assert2by2(_ mat: [[Double]]) {
+    let W = mat[0].count
+    let H = mat.count
+    assert(W == 2 && H == 2, "only supports 2 by 2 matrices")
+  }
+
+  private func det(_ mat: [[Double]]) -> Double {
+    self.assert2by2(mat)
+    let (a, b, c, d) = (mat[0][0], mat[0][1], mat[1][0], mat[1][1])
+    return (a * d) - (b * c)
+  }
+
+  private func inv(_ mat: [[Double]]) -> [[Double]]? {
+    self.assert2by2(mat)
+    let det = self.det(mat)
+    if det == 0 { return nil }
+    let (a, b, c, d) = (mat[0][0], mat[0][1], mat[1][0], mat[1][1])
+    return [
+        [ d / det, -b / det],
+        [-c / det,  a / det]
+    ]
+  }
+
+  private func mul(_ mat: [[Double]], _ vec: [Double]) -> [Double] {
+    let (a, b, c, d) = (mat[0][0], mat[0][1], mat[1][0], mat[1][1])
+    let (x, y) = (vec[0], vec[1])
+    return [
+        a * x + b * y,
+        c * x + d * y,
+    ]
+  }
+
+  // Solves the matrix equation Ax = B, returning x
+  private func solve(A: [[Double]], B: [Double]) -> [Double]? {
+      guard let invA = self.inv(A) else { return nil }
+      return self.mul(invA, B)
+  }
+
+  func parse(_ data: String) -> [(A: [[Double]], B: [Double])] {
+      let chunks = data.split(separator: "\n\n")
+      let results = chunks.map { chunk in
+          let rows = chunk.split(separator: "\n")
+          let matchesA = rows[0]
+              .matches(of: /\+(\d+)/)
+              .map { Double($0.output.1)! }
+          let matchesB = rows[1]
+              .matches(of: /\+(\d+)/)
+              .map { Double($0.output.1)! }
+          let matchesP = rows[2]
+              .matches(of: /=(\d+)/)
+              .map { Double($0.output.1)! }
+          let (Ax, Ay) = (matchesA[0], matchesA[1])
+          let (Bx, By) = (matchesB[0], matchesB[1])
+          let (Px, Py) = (matchesP[0], matchesP[1])
+          let A = [[Ax, Bx], [Ay, By]]
+          let B = [Px, Py]
+          return (A, B)
+      }
+      return results
+  }
+
+  func part1() -> Int {
+    let machines = self.parse(self.data)
+    var result: Double = 0
+    for machine in machines {
+        let (A, B) = machine
+        if let solution = self.solve(A: A, B: B) {
+            let (countA, countB) = (solution[0], solution[1])
+            if (
+                abs(countA - round(countA)) <= epsilon
+                && abs(countB - round(countB)) <= epsilon
+            ) {
+                result += (3 * countA + 1 * countB)
+            }
+        }
+    }
+    return Int(result)
+  }
+
+  // Had to play around (loosen) the tolerances a bit but we got there
+  func part2() -> Int {
+    let machines = self.parse(self.data)
+    var result: Double = 0
+    for machine in machines {
+        let (A, B) = machine
+        if let solution = self.solve(A: A, B: B.map { $0 + 10000000000000 }) {
+            let (countA, countB) = (solution[0], solution[1])
+            if (
+                abs(countA - round(countA)) <= epsilon
+                && abs(countB - round(countB)) <= epsilon
+            ) {
+                result += (3 * countA + 1 * countB)
+            }
+        }
+    }
+    return Int(result)
+  }
+}
+
+
+  // 94 * Na + 22 * Nb = 8400
+  // 34 * Na + 67 * Nb = 5400
+
+  // 94 22 Na = 8400
+  // 34 67 Nb   5400
+
+  // Na = inv(94 22 * 8400
+  // Nb       34 67)  5400
+
+// notes
+// - look up cramer's rule
+// - methods matter! solving for x in Ax + B as A^-1 * B is correct, but has
+// precision issues since i need to cast as a double when dividing by the
+// inverse. if i calculate the solution directly without casting, i wouldn't
+// have needed to juggle the epsilon value (i.e. the tolerance) to get the
+// correct answer...
